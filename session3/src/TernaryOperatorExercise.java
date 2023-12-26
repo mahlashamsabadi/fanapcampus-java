c2b1ce31ce6434d08d1077c2fd7fd5fb9e0bbae7
Mahla Shamsabadi <shamsabadimahla@gmail.com>
2023-12-26 09:12:20 +0330

    comments edited.

--- session3/src/TernaryOperatorExercise.java
+++ session3/src/TernaryOperatorExercise.java
@@ -1,14 +1,11 @@
 /**
- * اپراتور ternary در جاوا برای نوشتن دستور شرط به صورت یک خطی است.
- * این اپراتور به ما این امکان را میدهد که در صورت داشتن بلاک شرطی ساده آن را درون یک خط بنویسیم.
- * این کار به ساده نویسی و افزایش خوانایی کد کمک میکند.
- * نحوه نوشتن آن به این صورت است: result = (condition) ? expression1 : expression2
- * در این کد قسمت condition یک عبارت شرطی یا قطعه کدی است که خروجی boolean دارد.
- * در صورت برقرار بودن شرط نوشته شده در این قسمت مقدار نوشته شده در expression1 و در غیر این صورت مقدار expression2 در متغیر result ذخیره میشود.
+ * The ternary operator in Java is used to write conditional statements in one line.
+ * This operator allows us to write a simple conditional block in one line.
+ * This helps to simplify and increase the readability of the code.
+ * We should write it like this ==> result = (condition) ? expression1: expression2
+ * In this code, the condition part is a conditional expression or a piece of code that has a boolean output.
+ * If the condition written in this section is true, the express1 and otherwise the express2 will be saved in the result variable.
  */
-
-
-
 public class TernaryOperatorExercise {
     public static void main(String[] args) {
         int i = 10;
