fa93ca922044eea463c5d633695d13eef46d10c9
Mahla Shamsabadi <shamsabadimahla@gmail.com>
2024-01-02 12:30:09 +0330

    using wildcard for adding the elements of 2 lists.

--- session6/src/ListExercise.java
+++ session6/src/ListExercise.java
@@ -5,35 +5,29 @@ public class ListExercise {
 
     public static void main(String[] args) {
         List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
+        List<Double> list2 = new ArrayList<>();
         list1.add(1);
         list1.add(2);
-        list2.add(3);
-        list2.add(4);
-        list2.add(5);
+        list2.add(3.1);
+        list2.add(4.2);
+        list2.add(5.5);
 
-        for (Integer i : addElements(list1, list2)) {
-            System.out.println(i);
-        }
+        addElements(list1,list2);
         // output: 4,6
         System.out.println("---------------------------------------------------------------------");
         list1.add(6);
         list1.add(7);
         // list1: [1,2,6,7] , list2: [3,4,5]
-        for (Integer i : addElements(list1, list2)) {
-            System.out.println(i);
-        }
+        addElements(list1,list2);
         // output: 4,6,11,
 
     }
 
-    public static List<Integer> addElements(List<Integer> list1, List<Integer> list2) {
+    public static void addElements(List<? extends Number> list1, List<? extends Number> list2) {
         int minLength = Math.min(list1.size(), list2.size());
-        List<Integer> result = new ArrayList<>();
 
         for (int i = 0; i < minLength; i++) {
-            result.add(list1.get(i) + list2.get(i));
+            System.out.println((list1.get(i).doubleValue() + list2.get(i).doubleValue()));
         }
-        return result;
     }
 }
